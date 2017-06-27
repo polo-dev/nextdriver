edd3818606be03a519adc7e88c402a2edf9da44c
polo-dev <polo.reinhardt@gmail.com>
2017-06-27 08:44:15 +0200

    work error on connection

--- src/app/component/connection.component.ts
+++ src/app/component/connection.component.ts
@@ -7,5 +7,25 @@ import { Component } from '@angular/core';
 })
 
 export class ConnectionComponent {
-  title = 'Connexion';
+  title: string = 'Connexion';
+  email: string = '';
+  password: string = '';
+  error: string = null;
+  isClassVisible: true;
+  emailError: boolean = false;
+  passwordError: boolean = false;
+
+  submitLogin() {
+      console.log(this.email);
+      if(this.email.length < 1 || this.password.length < 1)
+      {
+        this.error = 'Mince alors';
+        this.emailError = (this.email.length < 2) ? true : false; 
+        this.passwordError = (this.password.length < 2) ? true : false; 
+      }
+      else
+      {
+        this.error = null;
+      }
+  }
 }
